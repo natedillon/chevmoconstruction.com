462dffb36423eb92a37e80875dac3257a0393e80
Nate Dillon <natedillon@gmail.com>
2013-03-08 19:07:07 -0600

    initialize repository

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+	
+		<title>Chevmo Construction</title>
+		
+		<meta charset="utf-8" />
+		
+		<meta name="description" content="Residential, Commercial, New Construction, Remodeling" />
+		<meta name="author" content="D. Nathan Dillon" />
+	
+		<style type="text/css">
+		
+			a:link, a:visited {
+				color: #00A3CB;
+			}
+			
+			.content {
+				margin: 40px 0;
+				text-align: center;
+			}
+			
+			.logo {
+				margin-bottom: 100px;
+			}
+			
+		</style>
+	
+	</head>
+	
+	<body>
+	
+		<div class="content">
+		
+			<h1 class="logo"><img src="logo.jpg" alt="Chevmo Construction, Broken Arrow, 918.904.9577" /></h1>
+			
+			<div class="facebook">
+				<div id="fb-root"></div>
+				<script>
+					(function(d, s, id) {
+						var js, fjs = d.getElementsByTagName(s)[0];
+						if (d.getElementById(id)) return;
+						js = d.createElement(s); js.id = id;
+						js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+						fjs.parentNode.insertBefore(js, fjs);
+					}
+					(document, 'script', 'facebook-jssdk'));
+				</script>
+				
+				<div class="fb-like" data-href="http://www.facebook.com/chevmoconstructionllc" data-send="true" data-width="450" data-show-faces="true"></div>
+			</div>
+			
+		</div>
+	
+	</body>
+
+</html>
