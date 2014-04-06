5e55fdae17258e2e4829c28e9e47043cb13ab8ea
Nate Dillon <natedillon@gmail.com>
2014-04-06 11:23:19 -0500

    added and styled social media links
    Also added Font Awesome for social icons, added more general text
    styling, and sized the content to fit on mobile phones.

--- index.php
+++ index.php
@@ -3,39 +3,92 @@
 <html lang="en">
 
 	<head>
-	
+
 		<title>Chevmo Construction</title>
-		
+
 		<meta charset="utf-8" />
-		
+
 		<meta name="description" content="Residential, Commercial, New Construction, Remodeling" />
 		<meta name="author" content="D. Nathan Dillon" />
-	
+
+		<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 		<style type="text/css">
-		
+
+			html {
+				font-size: 62.5%;
+			}
+
+			body {
+				font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+				font-size: 16px; font-size: 1.6rem;
+				line-height: 1.5;
+			}
+
 			a:link, a:visited {
 				color: #00A3CB;
 			}
-			
+
 			.content {
-				margin: 40px 0;
+				width: 460px;
+				margin: 40px auto;
 				text-align: center;
 			}
-			
+
 			.logo {
-				margin-bottom: 100px;
+				margin-bottom: 40px;
 			}
-			
+
+			.logo img {
+				width: 100%;
+			}
+
+			.social {
+				margin-bottom: 40px;
+			}
+
+			.social ul {
+				margin: 0;
+				padding: 0;
+				list-style-type: none;
+			}
+
+			.social ul li {
+				display: inline;
+				margin: 0 20px 0 0;
+				padding: 0;
+			}
+
+			.social a:link,
+			.social a:visited {
+				display: inline-block;
+				text-decoration: none;
+				white-space: no-wrap;
+			}
+
+			.social a:hover span,
+			.social a:active span {
+				text-decoration: underline;
+			}
+
 		</style>
-	
+
 	</head>
-	
+
 	<body>
-	
+
 		<div class="content">
-		
+
 			<h1 class="logo"><img src="logo.jpg" alt="Chevmo Construction, Broken Arrow, 918.904.9577" /></h1>
-			
+
+			<div class="social">
+				<ul>
+					<li><a href="http://facebook.com/chevmoconstructionllc"><i class="fa fa-facebook-square"></i> <span>Facebook</span></a></li>
+					<li><a href="http://twitter.com/chevmo_const"><i class="fa fa-twitter-square"></i> <span>Twitter</span></a></li>
+					<li><a href="http://instagram.com/chevmo_const"><i class="fa fa-instagram"></i> <span>Instagram</span></a></li>
+					<li><a href="http://pinterest.com/chevmoconst"><i class="fa fa-pinterest-square"></i> <span>Pinterest</span></a></li>
+				</ul>
+			</div> <!-- end .social -->
+
 			<div class="facebook">
 				<div id="fb-root"></div>
 				<script>
@@ -48,12 +101,12 @@
 					}
 					(document, 'script', 'facebook-jssdk'));
 				</script>
-				
+
 				<div class="fb-like" data-href="http://www.facebook.com/chevmoconstructionllc" data-send="true" data-width="450" data-show-faces="true"></div>
-			</div>
-			
+			</div> <!-- end .facebook -->
+
 		</div>
-	
+
 		<script type="text/javascript">
 
 			var _gaq = _gaq || [];
@@ -67,7 +120,7 @@
 			})();
 
 		</script>
-	
+
 	</body>
 
 </html>
